ac67649f792e206015f19740248925368a76e311
Abhay Singh <128287855+Er-Abhay-FSD@users.noreply.github.com>
2024-07-22 09:08:09 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="images/icon.png">
+    <title>Title</title>
+    <!-- custom-theme -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Coalition Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript">
+        addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); }
+    </script>
+    <!-- //custom-theme -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- js -->
+    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+    <!-- //js -->
+    <!-- font-awesome-icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome-icons -->
+    <link href="//fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<!-- banner -->
+<div class="banner1">
+    <div class="container">
+        <div class="w3_agile_banner_top">
+            <div class="agile_phone_mail">
+                <ul>
+                    <li><i class="fa fa-phone" aria-hidden="true"></i>+(91) 6392784651</li>
+                    <li><i class="fa fa-envelope" aria-hidden="true"></i><a href="abhaysingh0836481@outlook.com">abhaysingh0836481@outlook.com</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="agileits_w3layouts_banner_nav">
+            <nav class="navbar navbar-default">
+                <div class="navbar-header navbar-left">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <h1><a class="navbar-brand" href="index.php"><img src="images/logo.png" class="img-responsive"></a></h1>
+                </div>
+                <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                    <nav class="cl-effect-13" id="cl-effect-13">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.php">Home</a></li>
+                            <li class="active"><a href="about.php">About</a></li>
+                            <li><a href="portfolio.php">Products</a></li>
+                            <li><a href="blog.php">Blog</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
+<!-- //banner -->
+<!-- about -->
+<div class="about-wthree">
+    <h2 class="w3l_head w3l_head1">About Us</h2>
+    <div class="furniture">
+        <div class="container">
+            <p>
+                <!-- About content -->
+            </p>
+        </div>
+    </div>
+</div>
+<!-- //about -->
+<!-- statistics -->
+<div class="statistics">
+    <div class="container">
+        <h3 class="w3l_head w3l_head1">Our Statistics</h3>
+        <div class="statistics-grids">
+            <div class="col-md-3 statistics-grid">
+                <div class="counter numscroller"> 190</div>
+                <h5>Active users</h5>
+            </div>
+            <div class="col-md-3 statistics-grid">
+                <div class="counter numscroller">8</div>
+                <h5>Partners</h5>
+            </div>
+            <div class="col-md-3 statistics-grid">
+                <div class="counter numscroller">10520</div>
+                <h5>Raised Fundings</h5>
+            </div>
+            <div class="col-md-3 statistics-grid">
+                <div class="counter numscroller">2536</div>
+                <h5>Questions</h5>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<!-- //statistics -->
+<!-- blog -->
+<div class="blog">
+    <div class="container">
+        <h3 class="w3l_head w3l_head1">Our Blog</h3>
+        <div class="blog-grids">
+            <div class="col-md-4 blog-grid">
+                <div class="blog-img">
+                    <img src="images/blog1.jpg" alt="Blog Image" class="img-responsive" />
+                </div>
+                <div class="blog-content">
+                    <h4><a href="blog-single.php">Life in Our Hostel</a></h4>
+                    <p>Experience the best community living and make lifelong friends in our hostel. Read more about the vibrant life here.</p>
+                    <a href="blog-single.php" class="read-more">Read More</a>
+                </div>
+            </div>
+            <div class="col-md-4 blog-grid">
+                <div class="blog-img">
+                    <img src="images/blog2.jpg" alt="Blog Image" class="img-responsive" />
+                </div>
+                <div class="blog-content">
+                    <h4><a href="blog-single.php">Events and Activities</a></h4>
+                    <p>Join our numerous events and activities designed to enrich your stay and provide a dynamic living experience.</p>
+                    <a href="blog-single.php" class="read-more">Read More</a>
+                </div>
+            </div>
+            <div class="col-md-4 blog-grid">
+                <div class="blog-img">
+                    <img src="images/blog3.jpg" alt="Blog Image" class="img-responsive" />
+                </div>
+                <div class="blog-content">
+                    <h4><a href="blog-single.php">Hostel Facilities</a></h4>
+                    <p>Discover the top-notch facilities we offer to ensure a comfortable and enjoyable stay for all our residents.</p>
+                    <a href="blog-single.php" class="read-more">Read More</a>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<!-- //blog -->
+<?php
+    include("footer.php");
+?>
+</body>
+</html>
